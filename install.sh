96e05fd2bcc9468c1f52dfe9e207bfbc70e27053
Joshua Raphael <josh.n.raphael@gmail.com>
2025-03-31 18:30:10 -0700

    try 7zip extraction

--- install.sh
+++ install.sh
@@ -12,30 +12,24 @@ GAME_CAPTURE_SLUG=gchdm_25_1119
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install git dmg2img libusb-dev clang make build-essential cmake libusb-1.0 pkg-config -y
+sudo apt-get install git libusb-dev clang make build-essential cmake libusb-1.0 pkg-config 7zip p7zip-full -y
 rm -rf firmware
-sudo umount dmg || /bin/true
 rm -rf dmg
 rm -rf build
 rm -f ${GAME_CAPTURE_SLUG}.dmg
 rm -f ${GAME_CAPTURE_SLUG}.dmg.img
 sudo rm -rf /usr/local/lib/firmware/gchd
 mkdir -p firmware
-sudo modprobe hfsplus
 curl --output ${GAME_CAPTURE_SLUG}.dmg --location "https://edge.elgato.com/egc/macos/egcm/${GAME_CAPTURE_VERSION}/final/${GAME_CAPTURE_SLUG}.dmg"
-dmg2img ${GAME_CAPTURE_SLUG}.dmg -o ${GAME_CAPTURE_SLUG}.dmg.img
 mkdir -p dmg
-sudo mount ${GAME_CAPTURE_SLUG}.dmg.img dmg/
-sudo dmesg
+7z x -odmg -y '-x!*/Applications*' gchdm_25_1119.dmg
 sudo mkdir -p /usr/local/lib/firmware/gchd
 ls -la dmg/
-sudo cp dmg/Game\ Capture\ HD.app/Contents/Resources/Firmware/Beddo/mb86h57_h58_idle.bin /usr/local/lib/firmware/gchd/
-sudo cp dmg/Game\ Capture\ HD.app/Contents/Resources/Firmware/Beddo/mb86h57_h58_enc_h.bin /usr/local/lib/firmware/gchd/
-sudo cp dmg/Game\ Capture\ HD.app/Contents/Resources/Firmware/Beddo/mb86m01_assp_nsec_idle.bin /usr/local/lib/firmware/gchd/
-sudo cp dmg/Game\ Capture\ HD.app/Contents/Resources/Firmware/Beddo/mb86m01_assp_nsec_enc_h.bin /usr/local/lib/firmware/gchd/
+sudo cp dmg/Game\ Capture\ HD/Game\ Capture\ HD.app/Contents/Resources/Firmware/Beddo/mb86h57_h58_idle.bin /usr/local/lib/firmware/gchd/
+sudo cp dmg/Game\ Capture\ HD/Game\ Capture\ HD.app/Contents/Resources/Firmware/Beddo/mb86h57_h58_enc_h.bin /usr/local/lib/firmware/gchd/
+sudo cp dmg/Game\ Capture\ HD/Game\ Capture\ HD.app/Contents/Resources/Firmware/Beddo/mb86m01_assp_nsec_idle.bin /usr/local/lib/firmware/gchd/
+sudo cp dmg/Game\ Capture\ HD/Game\ Capture\ HD.app/Contents/Resources/Firmware/Beddo/mb86m01_assp_nsec_enc_h.bin /usr/local/lib/firmware/gchd/
 mkdir -p build
 cd build
 cmake ..
 make
-cd ..
-sudo umount dmg
