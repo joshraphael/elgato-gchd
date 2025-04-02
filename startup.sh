1f0501485d8ac40142c1d08afcde41d5e89ccec9
Joshua Raphael <josh.n.raphael@gmail.com>
2025-04-01 22:47:29 -0700

    add socket startup script

--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo ./build/src/gchd -color-space rgb
+sudo ./build/src/gchd -color-space rgb # FIFO -- open /tmp/gchd.ts in VLC
+
+# sudo ./build/src/gchd -f socket -color-space rgb # Socket - shell script: ffplay udp://0.0.0.0:57384
